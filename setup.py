86afe5c65d121d2d0e4f6f282d1a39fe6af8f8e4
VaibhavY <vaibhav.yaramwar@gmail.com>
2021-08-04 23:09:50 +0530

    running code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Vaibhav Yaramwar",
+    description = "its a wine Q package",
+    packages = find_packages(),
+    license = "MIT"
+
+)
