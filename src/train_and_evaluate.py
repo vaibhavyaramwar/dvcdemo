3c029673910f35c08397e16bd679a45c72b04305
VaibhavY <vaibhav.yaramwar@gmail.com>
2021-08-06 21:46:04 +0530

    Final Code

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,12 +1,13 @@
-# load train and test
+# load the train and test
 # train algo
-# save the matrices and params
-
-
+# save the metrices, params
 import os
-import numpy as np
+import warnings
+import sys
 import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from get_data import read_params
 import argparse
@@ -14,22 +15,22 @@ import joblib
 import json
 
 
-def eval_metrics(actual, prediction):
-    rmse = np.sqrt(mean_squared_error(actual, prediction))
-    mae = mean_absolute_error(actual, prediction)
-    r2 = r2_score(actual, prediction)
-
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
     return rmse, mae, r2
-
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
     train_data_path = config["split_data"]["train_path"]
     random_state = config["base"]["random_state"]
-    model_directory = config["model_dir"]
+    model_dir = config["model_dir"]
+
     alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
     l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+
     target = [config["base"]["target_col"]]
 
     train = pd.read_csv(train_data_path, sep=",")
@@ -37,24 +38,26 @@ def train_and_evaluate(config_path):
 
     train_y = train[target]
     test_y = test[target]
-    train_X = train.drop(target, axis=1)
-    test_X = test.drop(target, axis=1)
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
-    lr.fit(train_X, train_y)
+    train_x = train.drop(target, axis=1)
+    test_x = test.drop(target, axis=1)
 
-    predicted_quality = lr.predict(test_X)
-    print("Total Prediction dataset length : ", len(predicted_quality))
-    print("Total Actual dataset length : ", len(test_y))
+    lr = ElasticNet(
+        alpha=alpha, 
+        l1_ratio=l1_ratio, 
+        random_state=random_state)
+    lr.fit(train_x, train_y)
 
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_quality)
+    predicted_qualities = lr.predict(test_x)
+    
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
     print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
     print("  RMSE: %s" % rmse)
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
 
-    #####################################################
+#####################################################
     scores_file = config["reports"]["scores"]
     params_file = config["reports"]["params"]
 
@@ -72,17 +75,18 @@ def train_and_evaluate(config_path):
             "l1_ratio": l1_ratio,
         }
         json.dump(params, f, indent=4)
-    #####################################################
+#####################################################
 
-    os.makedirs(model_directory, exist_ok=True)
-    model_path = os.path.join(model_directory, "model.joblib")
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
 
     joblib.dump(lr, model_path)
 
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     args = argparse.ArgumentParser()
-    # path = os.path.join("..", "params.yaml")
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     train_and_evaluate(config_path=parsed_args.config)
--- template.py
+++ template.py
@@ -1,30 +1,27 @@
 import os
 
+
 dirs = [
     os.path.join("data", "raw"),
-    os.path.join("data", "processed"),
+    os.path.join("data","processed"),
     "notebooks",
     "saved_models",
     "src"
-
 ]
 
-for dirs_ in dirs:
-    os.makedirs(dirs_, exist_ok=True)
-    with open(os.path.join(dirs_, ".gitkeep"),"w") as f:
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
         pass
 
-files = [
 
+files = [
     "dvc.yaml",
     "params.yaml",
     ".gitignore",
     os.path.join("src","__init__.py")
-
 ]
 
 for file_ in files:
-    with open(file_,"w") as f:
+    with open(file_, "w") as f:
         pass
-
-
